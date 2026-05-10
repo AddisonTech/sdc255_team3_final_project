8b198011b0a602b6af695dfc3c9cb989fae058f2
adamsmithhisler <adasmi6974@students.ecpi.edu>
2026-05-10 14:50:19 -0500

    Add files via upload
    Add Calculation 1 function

---
+++ calculations.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "calculations.h"
+
+/*
+    Calculation 1 Function
+    Author: Adam Smithhisler
+
+    Purpose:
+    This function accepts two numbers, calculates their average,
+    and returns the result.
+*/
+
+double calculationOne(double num1, double num2) {
+    double result = (num1 + num2) / 2;
+    return result;
+}
---
+++ calculations.h
@@ -0,0 +1,6 @@
+#ifndef CALCULATIONS_H
+#define CALCULATIONS_H
+
+double calculationOne(double num1, double num2);
+
+#endif
